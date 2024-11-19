b56db79f93a6bc71d7bb224f803d60e2a6423b4d
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-11-19 18:12:07 +0530

    add new resume link

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/AnjanaTharanga24",
   resume:
-    "https://drive.google.com/drive/u/2/folders/1PjB1INaoV5brnwAWQOuUFRgLYmseZf9a",
+    "https://drive.google.com/file/d/1xQdPvS5anpLnpy03QkbTPFCqcsvqYo_U/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/anjana-tharanga-99709228b/",
   twitter: "",
   insta: "",
